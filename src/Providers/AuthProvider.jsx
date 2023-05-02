573b2dee8cdbbf0ca8a3e3310d5d2f9b4242555c
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-05-03 02:17:40 +0600

    set spinner

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -10,17 +10,22 @@ const auth = getAuth(app);
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
 
   
 
     const createUser =  (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
     const signIn = (email, password) =>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () =>{
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -28,6 +33,7 @@ const AuthProvider = ({children}) => {
        const unsubscribe =  onAuthStateChanged(auth, loggedUser =>{
             console.log('logged in user inside auth state observer', loggedUser);
             setUser(loggedUser);
+            setLoading(false);
         })
 
         return () =>{
@@ -36,6 +42,7 @@ const AuthProvider = ({children}) => {
     },[])
     const authInfo = { 
         user,
+        loading,
         createUser,
         signIn,
         logOut
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
 
 
 const PrivateRoute = ({children}) => {
-    const {user} = useContext(AuthContext);
+    const {user, loading} = useContext(AuthContext);
     const location = useLocation();
     console.log(location);
    
+    if(loading){
+        return <Spinner animation="border" variant="primary" />
+    }
 
     if(user){
         return children;
@@ -26,6 +30,7 @@ export default PrivateRoute;
  * 2. if user is looged in, then allow them to visit the route
  * 3. Else redirect the user to the login page.
  * 4. Setup the private router
+ * 5. Handle Loading
  * 
  * 
  * **/ 
