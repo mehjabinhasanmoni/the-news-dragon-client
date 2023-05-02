a7db6f9e45b09fdabb923ffffda1c053b5028400
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-05-02 16:56:01 +0600

    News card, ratings, icons, moment

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import Header from '../pages/Shared/Header/Header';
-import Footer from '../pages/Shared/Footer/Footer';
 import { Col, Container, Row } from 'react-bootstrap';
 import LeftNav from '../pages/Shared/LeftNav/LeftNav';
-import RightNav from '../pages/Shared/RightNav/RightNav';
 import { Outlet } from 'react-router-dom';
+import RightNav from '../pages/Shared/RightNav/RightNav';
+import Footer from '../pages/Shared/Footer/Footer';
 
-const Main = () => {
-     return (
+const NewsLayout = () => {
+    return (
         <div>
-            <Header></Header>
+             <Header></Header>
             <Container>
                 
                 <Row>
-                    <Col lg={3}>
-                      <LeftNav>
-
-                      </LeftNav>
-
-                    </Col>
-
-                    <Col lg={6}>
+                   
+                    <Col lg={9}>
                         
                         <Outlet></Outlet>
 
@@ -39,4 +33,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default NewsLayout;
