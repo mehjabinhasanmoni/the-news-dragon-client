cc8387e490de3408719e79ac6f2a14490da32770
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-05-03 03:57:05 +0600

    deploy client & server in firebasse & vercel

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 import {  Card, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
-import Rating from 'react-rating';
+//import Rating from 'react-rating';
+import { Rating } from '@smastrom/react-rating';
+
+import '@smastrom/react-rating/style.css'
 
 const NewsCard = ({news}) => {
     const {_id, title, details, image_url, author, total_view, rating } = news;
@@ -31,15 +34,16 @@ const NewsCard = ({news}) => {
                 
             </Card.Body>
             <Card.Footer className="text-muted d-flex">
-                <div className='flex-grow-1'>
-                <Rating
+                <div className='flex-grow-1 d-flex align-items-center'>
+                {/* <Rating
                     placeholderRating={rating.number}
                     readonly
                     emptySymbol={<FaRegStar></FaRegStar>}
                     placeholderSymbol={<FaStar className='text-danger'></FaStar>}
                     fullSymbol={<FaStar></FaStar>}
-                    />
-                    <span>{rating?.number}</span>
+                    /> */}
+                    <Rating style={{ maxWidth: 150 }} value={rating?.number} readOnly />
+                    <span className='ms-2'>{rating?.number}</span>
                 </div>
                <div>
                     <FaEye></FaEye> {total_view}
